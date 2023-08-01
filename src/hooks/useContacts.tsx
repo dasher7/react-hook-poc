796d0f2411fc4873d642461957848ce182a4c414
Andrea Bredice <bredice.andrea@gmail.com>
2023-08-01 11:04:28 +0200

    added eslint config

--- src/hooks/useContacts.tsx
+++ src/hooks/useContacts.tsx
@@ -1,11 +1,11 @@
-
+import React from "react"
 /**
  * This hook can be used to store and then create a list of contacts.
  * You can hook this function to a component, or to a parent component.
  * We will use this into view components that own a form and the checkboxes to create a list of contacts.
  * @returns the list of contacts that were created
  */
-export const useContacts = () => {
+export const useContacts: React.FC = () => {
   return (
     <div>useContacts</div>
   )
