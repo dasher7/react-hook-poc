25c6b22b6ec957b6673c28cc36d2f29daf56bb5b
Andrea Bredice <bredice.andrea@gmail.com>
2023-08-01 10:52:07 +0200

    added jsdocs and better-docs and also scaffolded folders

---
+++ src/hooks/useContacts.tsx
@@ -0,0 +1,12 @@
+
+/**
+ * This hook can be used to store and then create a list of contacts.
+ * You can hook this function to a component, or to a parent component.
+ * We will use this into view components that own a form and the checkboxes to create a list of contacts.
+ * @returns the list of contacts that were created
+ */
+export const useContacts = () => {
+  return (
+    <div>useContacts</div>
+  )
+}
